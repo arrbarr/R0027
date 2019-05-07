6d74f6560b69bf53ad9f95f80a3bd3d623efe166
Arttu <48829078+arrbarr@users.noreply.github.com>
2019-05-07 09:46:28 +0300

    Update jawakening.java
    new version

--- jawakening.java
+++ jawakening.java
@@ -5,18 +5,79 @@ import java.util.Scanner;
 public class jawakening {
 		
 		static int valinta;
+		static String hahmoNimi;
+		static int vaikeustaso;
 		static Scanner lukija = new Scanner(System.in);
-		static String nimi;
-		static int elämät = 3;
+		static Scanner vaikeustasoLukija = new Scanner(System.in);
+		static Scanner nimiLukija = new Scanner(System.in);
+		
 		
 	public static void main(String[] args) {
 		
-		System.out.println("Javakening©");
+		System.out.println("\r\n" + 
+				"\r\n" + 
+				"     _  ___      ___   _  _____ _  _ ___ _  _  ___ \r\n" + 
+				"  _ | |/_\\ \\    / /_\\ | |/ / __| \\| |_ _| \\| |/ __|\r\n" + 
+				" | || / _ \\ \\/\\/ / _ \\| ' <| _|| .` || || .` | (_ |\r\n" + 
+				"  \\__/_/ \\_\\_/\\_/_/ \\_\\_|\\_\\___|_|\\_|___|_|\\_|\\___|\r\n" + 
+				"                                                   \r\n" + 
+				"\r\n" + 
+				"");
+		System.out.println("		***Created by: ***");
+		System.out.println("	  Arttu Pahkin & Ville Jaatinen ");
+		System.out.println("");
 		
-		System.out.println("Heräät kosteasta ojanpenkasta. \nOlet pukeutunut likaisiin housuihin ja ohueen paitaan. \nEt muista mitään mutta repussasi lukee nimi (Syötä nimesi)");
-		nimi = lukija.nextLine();
+		//Pyydetään käyttäjältä hahmon nimi
 		
-		System.out.println("\"" + nimi + "… olenko se todella minä?” ajattelet kun nouset jaloillesi ja näet vieressä olevan tien vievän syvälle metsään."
+		
+		System.out.println("Nimeä hahmosi:");
+		hahmoNimi = nimiLukija.nextLine();
+		
+		System.out.println("Hei " + hahmoNimi + "! Aloitetaan seikkailu!");
+		System.out.println();
+
+		//Vaikeustason valinta
+		
+		System.out.println("Vaikeustasot:\n" + "1. Normaali\n" + "2. Zero Deaths");
+		vaikeustaso = vaikeustasoLukija.nextInt();
+
+		//Kysytään kunnes käyttäjä antaa kelvollisen syötteen
+		if(vaikeustaso >2 || vaikeustaso<1)
+		
+		do {
+
+			System.out.println("Valinta ei ole kelvollinen, käytä numeroita 1-2 vaikeustason valitsemiseen.");
+			vaikeustaso = vaikeustasoLukija.nextInt();
+
+		} while (vaikeustaso > 2 || vaikeustaso < 1);
+		
+		//Talletetaan valinta muuttujaan vaikeustaso. Muuttujan arvo = elämien määrä (hyödynnetään for loopissa)
+
+		if (vaikeustaso == 1 || vaikeustaso == 2)
+			switch (vaikeustaso) {
+
+			case 1:
+				vaikeustaso = 5;
+
+				System.out.println("Normaali vaikeustaso valittu.");
+				break;
+
+			case 2:
+				vaikeustaso = 1;
+
+				System.out.println("Zero Deaths valittu. Sinulta ei ainakaan puutu rohkeutta...");
+				break;
+			}
+		
+		System.out.println();
+		
+		System.out.println("[PAINA ENTER ALOITTAAKSESI PELIN]");
+		lukija.nextLine();
+		lukija.nextLine();
+		
+		System.out.println("Heräät kosteasta ojanpenkasta. \nOlet pukeutunut likaisiin housuihin ja ohueen paitaan. \nEt muista mitään mutta repussasi lukee nimi");
+		
+		System.out.println("\"" + hahmoNimi + "… olenko se todella minä?” ajattelet kun nouset jaloillesi ja näet vieressä olevan tien vievän syvälle metsään."
 				+ "\nMetsän lumo houkuttelee sinut sitä päin ja ennen kun huomaatkaan, olet keskellä harmaata, pimeää metsää.");
 		System.out.println();
 		System.out.println("Näet pimeän polun jakaantuvan kahtia edessäsi. "
@@ -71,15 +132,15 @@ public class jawakening {
 			System.out.println("Kysyt miehiä karsimaan lastia ja pienen suostuttelun jälkeen he heittävät muutaman kassin rannalle. "
 					+ "\nMiehet saavat työnnettyä veneen veteen mutta heti kun yrittävät hypätä kyytiin, vene kaatuu ja uppoaa. "
 					+ "\nVihaiset miehet kaivavat puukot taskuistaan ja puukottavat sinua.");
-			System.out.println("[Elämiä jäljellä: "+ (elämät-1) + "]");
+			System.out.println("[Elämiä jäljellä: "+ (vaikeustaso-1) + "]");
 			System.out.println("[PAINA ENTER]");
 			lukija.nextLine();
 			lukija.nextLine();
-			elämät = elämät - 1;
-			if (elämät>0) {
+			vaikeustaso = vaikeustaso - 1;
+			if (vaikeustaso>0) {
 				oikeaPolkuTehtävä1();
 			}
-			if (elämät==0) {
+			if (vaikeustaso==0) {
 				peliohi();
 			}
 		}
@@ -105,15 +166,15 @@ public class jawakening {
 		}
 		else if (valinta==2) {
 			System.out.println("Hyvin nopeasti vyöryvä yöpakkanen jäädyttää sinut kuoliaaksi jauhopussien viereen.");
-			System.out.println("[Elämiä jäljellä: "+ (elämät-1) + "]");
+			System.out.println("[Elämiä jäljellä: "+ (vaikeustaso-1) + "]");
 			System.out.println("[PAINA ENTER]");
 			lukija.nextLine();
 			lukija.nextLine();
-			elämät = elämät - 1;
-			if (elämät>0) {
+			vaikeustaso = vaikeustaso - 1;
+			if (vaikeustaso>0) {
 				oikeaPolkuTehtävä2();
 			}
-			if (elämät==0) {
+			if (vaikeustaso==0) {
 				peliohi();
 			}
 		}
@@ -146,15 +207,15 @@ public class jawakening {
 					+ "\nMerituuli puhaltaa osan pois mutta jäljelle jäävään kukkulaan heität sytytetyn tulitikun. "
 					+ "\nTulitikku sammuu välittömästi ja yrität uudelleen, kunnes aski on tyhjä. "
 					+ "\nPimeys on jo laskeutunut ja kuulet susilauman puurajassa. \nPakenet merelle mutta et muista miten uidaan ja hukut nopeasti.");
-			System.out.println("[Elämiä jäljellä: "+ (elämät-1) + "]");
+			System.out.println("[Elämiä jäljellä: "+ (vaikeustaso-1) + "]");
 			System.out.println("[PAINA ENTER]");
 			lukija.nextLine();
 			lukija.nextLine();
-			elämät = elämät - 1;
-			if (elämät>0) {
+			vaikeustaso = vaikeustaso - 1;
+			if (vaikeustaso>0) {
 				oikeaPolkuTehtävä3();
 			}
-			if (elämät==0) {
+			if (vaikeustaso==0) {
 				peliohi();
 			}
 		}
@@ -163,15 +224,15 @@ public class jawakening {
 					+ "\nYrität täristen sytyttää kaapua tuleen mutta katkaiset kaksi ensimmäistä tulitikkua. "
 					+ "\nViimeinen syttyy ja onnistut sytyttämään kaapusi tuleen. \nSe tarjoama valo paljastaa loistavia silmäpareja puunrajassa. "
 					+ "\nPaalu palaa loppuun hyvin nopeasti ja pimeyden langetessa sudet hyökkäävät ja raatelevat sinut.");
-			System.out.println("[Elämiä jäljellä: "+ (elämät-1) + "]");
+			System.out.println("[Elämiä jäljellä: "+ (vaikeustaso-1) + "]");
 			System.out.println("[PAINA ENTER]");
 			lukija.nextLine();
 			lukija.nextLine();
-			elämät = elämät - 1;
-			if (elämät>0) {
+			vaikeustaso = vaikeustaso - 1;
+			if (vaikeustaso>0) {
 				oikeaPolkuTehtävä3();
 			}
-			if (elämät==0) {
+			if (vaikeustaso==0) {
 				peliohi();
 			}
 		}
@@ -209,30 +270,30 @@ public class jawakening {
 		else if (valinta==2) {
 			System.out.println("Otat panikoiden tulessa olevan oksan nuotiosta ja huidot sitä läheneviä susia kohti. "
 					+ "\nVoimakas merituuli ja huidontasi sammuttavat oksan nopeasti ja sudet hyökkäävät kimppuusi.");
-			System.out.println("[Elämiä jäljellä: "+ (elämät-1) + "]");
+			System.out.println("[Elämiä jäljellä: "+ (vaikeustaso-1) + "]");
 			System.out.println("[PAINA ENTER]");
 			lukija.nextLine();
 			lukija.nextLine();
-			elämät = elämät - 1;
-			if (elämät>0) {
+			vaikeustaso = vaikeustaso - 1;
+			if (vaikeustaso>0) {
 				oikeaPolkuTehtävä4();
 			}
-			if (elämät==0) {
+			if (vaikeustaso==0) {
 				peliohi();
 			}
 		}
 		else if (valinta==3) {
 			System.out.println("Juokset pakoon kohti polkua mikä toi sinut tälle rannalle mutta pimeydessä et näe mihin astut ja kompastut kiveen. "
 					+ "\nSusilauma raatelee sinut.");
-			System.out.println("[Elämiä jäljellä: "+ (elämät-1) + "]");
+			System.out.println("[Elämiä jäljellä: "+ (vaikeustaso-1) + "]");
 			System.out.println("[PAINA ENTER]");
 			lukija.nextLine();
 			lukija.nextLine();
-			elämät = elämät - 1;
-			if (elämät>0) {
+			vaikeustaso = vaikeustaso - 1;
+			if (vaikeustaso>0) {
 				oikeaPolkuTehtävä4();
 			}
-			if (elämät==0) {
+			if (vaikeustaso==0) {
 				peliohi();
 			}
 		}
@@ -248,7 +309,7 @@ public class jawakening {
 				+ "\nPian polun varrella huomaat melko ränsistyneen mökin. "
 				+ "Savupiipusta tuleva savu viittaa, että hökötys on asuttu. \nKävelet mökin ovelle. "
 				+ "Koputat oveen kolmesti ja muutaman kymmenen sekunnin kuluttua ovi aukeaa hieman ja raosta kurkistaa vanhan miehen silmä. "
-				+ "\n”M-Mitäh? "+ nimi + "? Ei voi olla…” Ihmettelet miten tämä vanha mies tiesi nimesi. "
+				+ "\n”M-Mitäh? "+ hahmoNimi + "? Ei voi olla…” Ihmettelet miten tämä vanha mies tiesi nimesi. "
 				+ "\nMies kutsuu sinut sisään ja sieltä hohkaava lämpö houkuttelee sinut vastaanottamaan kutsun.");
 		System.out.println("”En voi uskoa, että palasit” mies sanoo.");
 		vasenPolkuTehtävä1();
@@ -260,7 +321,7 @@ public class jawakening {
 	private static void vasenPolkuTehtävä1() {
 		System.out.println("");
 		System.out.println("Mies katsoo sinua odottaen jonkinlaista kommunikaatiota.[Miten vastaat]");
-		System.out.println("1: \"Kuka sinä olet?\" \n2: \"Aa juu "+ nimi +" sehän olen minä\" \n3: *Tuijota miestä*");
+		System.out.println("1: \"Kuka sinä olet?\" \n2: \"Aa juu "+ hahmoNimi +" sehän olen minä\" \n3: *Tuijota miestä*");
 		System.out.println("");
 		valinta = lukija.nextInt();
 		
@@ -270,15 +331,15 @@ public class jawakening {
 		else if (valinta==1) {
 			System.out.println("Kysyt mieheltä, kuka hän on. \n”Mitä? Etkö muista? O-olen isäsi, Juhani” \nKatsot häntä pitkään yrittäen muistaa jotakin. "
 					+ "\nEt onnistu ja hän on edelleen tuntematon vanha mies. \nMies on näkyvästi pettynyt ja sanoo ehkä sinun olisi paras lähteä.");
-			System.out.println("[Elämiä jäljellä: "+ (elämät-1) + "]");
+			System.out.println("[Elämiä jäljellä: "+ (vaikeustaso-1) + "]");
 			System.out.println("[PAINA ENTER]");
 			lukija.nextLine();
 			lukija.nextLine();
-			elämät = elämät - 1;
-			if (elämät>0) {
+			vaikeustaso = vaikeustaso - 1;
+			if (vaikeustaso>0) {
 				vasenPolkuTehtävä1();
 			}
-			if (elämät==0) {
+			if (vaikeustaso==0) {
 				peliohi();
 			}
 		}
@@ -290,15 +351,15 @@ public class jawakening {
 					+ "\n”Tein tämän viime viikolla, en ole kerennyt kokeilemaan vielä.” Hän ojentaa pullon sinulle samalla kun et ole vieläkään lopettanut tuijottamista. "
 					+ "Hän juottaa sinulle kulauksen. "
 					+ "\nNäkösi pimenee ja kaadut maahan. Mies on järkyttynyt kuolleesta ruumista kotonaan ja juo itse pullon loppuun.");
-			System.out.println("[Elämiä jäljellä: "+ (elämät-1) + "]");
+			System.out.println("[Elämiä jäljellä: "+ (vaikeustaso-1) + "]");
 			System.out.println("[PAINA ENTER]");
 			lukija.nextLine();
 			lukija.nextLine();
-			elämät = elämät - 1;
-			if (elämät>0) {
+			vaikeustaso = vaikeustaso - 1;
+			if (vaikeustaso>0) {
 				vasenPolkuTehtävä1();
 			}
-			if (elämät==0) {
+			if (vaikeustaso==0) {
 				peliohi();
 			}
 		}
@@ -359,7 +420,12 @@ public class jawakening {
 	}
 
 	private static void peliohi() {
-		System.out.println("Peli ohi! Sinulta loppuivat elämät!");
+		System.out.println(" _   __ _   _  _____  _      _____  _____  _ \r\n" + 
+				"| | / /| | | ||  _  || |    |_   _||_   _|| |\r\n" + 
+				"| |/ / | | | || | | || |      | |    | |  | |\r\n" + 
+				"|    \\ | | | || | | || |      | |    | |  | |\r\n" + 
+				"| |\\  \\| |_| |\\ \\_/ /| |____ _| |_   | |  |_|\r\n" + 
+				"\\_| \\_/ \\___/  \\___/ \\_____/ \\___/   \\_/  (_)");
 		System.exit(0);
 	}
 
